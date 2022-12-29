b00dd2f78811a7d66f79b91f272deef4c6ed38ea
Om Amin <omamin135@gmail.com>
2022-12-29 15:54:38 -0500

    initial commit + webpack setup

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("Testing Config Setup");
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLG9DIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdG9kb19saXN0Ly4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKFwiVGVzdGluZyBDb25maWcgU2V0dXBcIik7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
