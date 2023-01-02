c40ed235be069eda60729b3f2c22810f90649052
Om Amin <omamin135@gmail.com>
2023-01-01 19:36:24 -0500

    add todo button adds a new todo card in the dom with the inputted fields

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { loadProjectsMenu, addNewProjectInput, replaceNewProjectInput} from "./D
 import Dir from "./manage_dir.js";
 import loadProjPage from "./DOM_management/render_active_project.js";
 import {loadCard, newCard, removeEditCard} from "./DOM_management/todo_card.js";
+import {Todo, Project} from "./project_classes.js";
 
 
 loadPage();
@@ -50,7 +51,14 @@ document.getElementById("new-todo").addEventListener("click", () => {
     newCard(Dir.projects[0].todo[0]);
     
     document.getElementById("submit-card").onclick = function processNewCard(){
+        Dir.addToActive(new Todo(document.querySelector(".edit-card.card-title").value,
+                                 document.querySelector(".edit-card.card-date").value,
+                                 document.querySelector(".edit-card.description").value,
+                                 "high"
+                                 ))
         removeEditCard();
+        console.log(Dir.activeProject());
+        loadCard(Dir.projects[Dir.activeProjIndex].todo[Dir.projects.length-1]);
     }
 })
 
